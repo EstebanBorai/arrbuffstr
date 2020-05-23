84c480bd3524176c364287b08bb83752d857a3e5
Esteban Borai <estebanborai@gmail.com>
2020-05-23 00:09:13 -0300

    Create make.ts

---
+++ src/arrbuffstr/make.ts
@@ -0,0 +1,22 @@
+import getEnv, { Env } from '../utils/get-env';
+import { toArrayBufferFactory, toStringFactory } from '../factory/index';
+
+interface ArrBuffStr {
+  toArrayBuffer: StringToArrayBuffer;
+  toString: ArrayBufferToString;
+}
+
+function make(): ArrBuffStr {
+  const currentEnv: Env = getEnv();
+
+  if (currentEnv === 'Node') {
+    throw new Error(`ArrBuffStr is not supported!`);
+  }
+
+  return {
+    toArrayBuffer: toArrayBufferFactory(Env.Browser),
+    toString: toStringFactory(Env.Browser)
+  };
+}
+
+export default make;
