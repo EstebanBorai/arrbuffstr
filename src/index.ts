941419c0b50f76ecd40aa3e6f75061ba59106022
Esteban Borai <estebanborai@gmail.com>
2020-05-23 00:10:33 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,3 @@
-import getEnv, { Env } from './utils/get-env';
-import { toArrayBufferFactory, toStringFactory } from './factory/index';
-
-interface ArrBuffStr {
-  toArrayBuffer: StringToArrayBuffer;
-  toString: ArrayBufferToString;
-}
-
-function make(): ArrBuffStr {
-  const currentEnv: Env = getEnv();
-
-  if (currentEnv === 'Node') {
-    throw new Error(`ArrBuffStr is not supported!`);
-  }
-
-  return {
-    toArrayBuffer: toArrayBufferFactory(Env.Browser),
-    toString: toStringFactory(Env.Browser)
-  };
-}
+import make from './arrstrbuff/make';
 
 export default make;
